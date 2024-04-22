144fc459d3a0b93298c2955d2c5b1283388d1f90
NikitaDmitruk <neomouz@icloud.com>
2024-04-22 16:11:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Scanner scanner = new Scanner(System.in);
+        User user1 = new User(1, "qwerty", "1234", new Status("VIP"));
+        User user2 = new User(2, "qwerty", "1234", new Status("VIP"));
+        User clone = null;
+        System.out.println("В базе данных имеются следующие пользователи: \n" + user1 + "\n" + user2 + "\nВведите id пользователя, которого хотите клонировать:");
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            if (id == 1) {
+                System.out.println("Какое клонирование Вы хотели бы выполнить? Введите 1 - для поверхностного, 2 - для глубокого.");
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    clone = user1.cloneShallow();
+                } else if (choice == 2) {
+                    clone = user1.cloneDeep();
+                } else {
+                    System.out.println("Вы ввели не то число!");
+                    return;
+                }
+            } else if (id == 2) {
+                System.out.println("Какое клонирование Вы хотели бы выполнить? Введите 1 - для поверхностного, 2 - для глубокого.");
+                int choice = scanner.nextInt();
+                if (choice == 1) {
+                    clone = user2.cloneShallow();
+                } else if (choice == 2) {
+                    clone = user2.cloneDeep();
+                } else {
+                    System.out.println("Вы ввели не то число!");
+                    return;
+                }
+            } else {
+                System.out.println("Такого id не существует!");
+                return;
+            }
+        } else {
+            System.out.println("Вы ввели не число!");
+            return;
+        }
+        System.out.println("Программа изменила статус у клона на common и id второго юзера на 1!");
+        clone.getStatus().setStatus("common");
+        user2.setId(1);
+        System.out.println(user1 + "\n" + user2 + "\nКлон: " + clone);
+        System.out.println("Результаты сравнения первого и второго юзера через ==: " + (user1 == user2));
+        System.out.println("Результаты сравнения первого и второго юзера через equals: " + user1.equals(user2));
+        System.out.println("Результаты сравнения первого юзера и клона через ==: " + (user1 == clone));
+        System.out.println("Результаты сравнения первого юзера и клона через equals: " + user1.equals(clone));
+        System.out.println("Результаты сравнения второго юзера и клона через ==: " + (user2 == clone));
+        System.out.println("Результаты сравнения второго юзера и клона через equals: " + user2.equals(clone));
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,82 @@
+import jdk.jshell.JShell;
+
+import java.util.Objects;
+
+public class User implements Cloneable {
+    private int id = 0;
+    private String login;
+    private String password;
+    private Status status;
+
+    public User(int id, String login, String password, Status status) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status.setStatus(status);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(status, user.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password, status);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", status=" + status +
+                '}';
+    }
+
+    protected User cloneShallow() throws CloneNotSupportedException {
+        return (User) super.clone();
+    }
+
+    protected User cloneDeep() throws CloneNotSupportedException {
+        User clone = (User) super.clone();
+        clone.status = this.status.clone();
+        return clone;
+    }
+}
